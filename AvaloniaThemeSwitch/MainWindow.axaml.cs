a2c2ce267d4e7b83e8b03c1f19d6d8e570531479
rabbitism <popmessiah@hotmail.com>
2024-05-29 14:46:58 +0800

    feat: utilize theme variant.

--- AvaloniaThemeSwitch/MainWindow.axaml.cs
+++ AvaloniaThemeSwitch/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Styling;
 
 namespace AvaloniaThemeSwitch;
 
@@ -7,5 +10,13 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void ThemeSwitch_OnIsCheckedChanged(object? sender, RoutedEventArgs e)
+    {
+        var app = Application.Current;
+        if (app is null) return;
+        var isDark = themeSwitch.IsChecked ?? false;
+        app.RequestedThemeVariant =  isDark ? ThemeVariant.Dark : ThemeVariant.Light;
     }
 }
